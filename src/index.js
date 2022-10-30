90f7dca16abd580b7675311981d257756f843189
ManavDaggula <manavdaggula@gmail.com>
2022-10-30 14:13:40 +0530

    import error removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import LoginForm from './LoginForm';
 import Login from "./Login"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
